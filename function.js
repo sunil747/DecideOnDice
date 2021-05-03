1a79fc20c240a2498b9906d4008e5cb244251521
sunil747 <30520358+sunil747@users.noreply.github.com>
2021-05-03 20:25:28 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,27 @@
+// alert("script ran")
+function changeOnClick(){
+  // generate a random number between 1-6
+  var random_number1 = Math.floor(Math.random()*6)+1;
+  var random_number2 = Math.floor(Math.random()*6)+1;
+  // select the image based on the random number
+  var randomDiceImage1 = "images/dice"+random_number1+".png"
+  var randomDiceImage2 = "images/dice"+random_number2+".png"
+  // change the images by query selector
+  image1_tag = document.querySelectorAll("img")[0];
+  image1_tag.setAttribute("src",randomDiceImage1)
+  image2_tag = document.querySelectorAll("img")[1];
+  image2_tag.setAttribute("src",randomDiceImage2)
+
+  // logic begins here to see who wins
+  if(random_number1>random_number2){
+    document.getElementById("tag_line").innerHTML = "You should go with option1";
+  }
+  else if (random_number1 < random_number2) {
+    document.getElementById("tag_line").innerHTML = "You should go with option2";
+  }
+  else
+  {
+    document.getElementById("tag_line").innerHTML = "Its a tie! Try again";
+  }
+
+}
